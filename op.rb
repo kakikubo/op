7d27b2047a823d0e532ac4d06fd3b5e03b8a0b7c
kakikubo teruo <kakikubo@gmail.com>
2012-05-15 18:23:24 +0900

    classにした

--- op.rb
+++ op.rb
@@ -1,28 +1,44 @@
 # -*- coding: utf-8 -*-
 op1 = [
-"　　　 _　　　 おっ　\n",
-"　　(　゜∀゜)　　　 \n",
-"　　(　 ⊂彡　　　　 \n",
-"　 　|　　　|　 　　 \n",
-"　 　し ⌒Ｊ　　　　 \n"
-]
+       "　　　 _　　　 おっ　\n",
+       "　　(　゜∀゜)　　　 \n",
+       "　　(　 ⊂彡　　　　 \n",
+       "　 　|　　　|　 　　 \n",
+       "　 　し ⌒Ｊ　　　　 \n"
+      ]
 op2 = 
-[
-"　　　 _ 　∩　　\n",
-"　　(　゜∀゜)彡 ぱい\n",
-"　　(　 　　|　　　　\n",
-"　 　|　　　|　　　　\n",
-"　 　し ⌒Ｊ　　　　 \n"
-]
+  [
+   "　　　 _ 　∩　　\n",
+   "　　(　゜∀゜)彡 ぱい\n",
+   "　　(　 　　|　　　　\n",
+   "　 　|　　　|　　　　\n",
+   "　 　し ⌒Ｊ　　　　 \n"
+  ]
 
-def say_op(op)
-  system("clear")
-  op.each {|v| print v }
-  sleep 0.5
+
+
+class Op
+
+
+  @array = []
+
+  def initialize(ar = [])
+    @array = ar
+  end
+
+  def say_op
+    system("clear")
+    @array.each {|v| print v ; v = " " + v }
+    sleep 0.5
+  end
 end
 
+
+o1 = Op.new(op1)
+o2 = Op.new(op2)
+
 while (true)
-  say_op(op1)
-  say_op(op2)
+  o1.say_op
+  o2.say_op
 end
 
